e2222983c74c139c7bdbfa4f41e2874ad5ee322f
dirkschub <56308889+dirkschub@users.noreply.github.com>
2022-03-28 18:45:59 +0200

    Update git-setup.sh
    update

--- git-setup.sh
+++ git-setup.sh
@@ -24,15 +24,21 @@
 # git push --set-upstream origin master
 # git reset --hard orgin/master
 
-
+project=$(basename `pwd`)
+echo "-----------------------------------------------------------------------------"
+echo "this is project https://github.com/dirkschub/"$project
+echo "-----------------------------------------------------------------------------"
 git config --global pull.rebase false
 git config --global user.name "dirkschub"
 git config --global user.email "dirkschub@gmx.de"
 sudo git config --system core.editor nano
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=32000'
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=32000'
 git config --global push.default simple
 
+git remote set-url origin git@github.com:dirkschub/$project
+
+echo "Everything set"
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
